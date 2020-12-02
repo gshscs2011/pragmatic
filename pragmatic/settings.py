3bd31f29bcabff81659737adfc0c0f28f5465349
gshscs2011 <gshscs2011@gmail.com>
2020-12-03 00:37:10 +0900

    Del reduntand files

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
 from pathlib import Path
-import environ
+import os, environ
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # reading .env file
 environ.Env.read_env(
 
-    env_file = os.path.join(BASE_DIR, '../.env')
+    env_file = os.path.join(BASE_DIR, '.env')
 )
 
 # Quick-start development settings - unsuitable for production
